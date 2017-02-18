075bedbd4896c6883b03f82a642ceaa07cbeabb6
mikerames <miguel.ramos@msn.com>
2017-02-18 01:52:09 +0000

    tst

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://<dbuser>:<dbpassword>@ds011873.mlab.com:11873/heroku_fqk7wsks',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
