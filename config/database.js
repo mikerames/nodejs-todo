ab0e7f3e4597da03c7950f3b69aa2116332716a8
Miguel Ramos <miguel.ramos@msn.com>
2019-07-29 10:55:03 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-	herokuUrl : 'mongodb://heroku_mk7qw61c:Aireverse2@ds261040.mlab.com:61040/heroku_mk7qw61c',
+	herokuUrl : 'mongodb://tst:Aireverse2@ds261040.mlab.com:61040/heroku_mk7qw61c',
     remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
