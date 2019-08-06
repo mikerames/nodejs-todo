b062573ebd193881124f92fef041952b86c29c2a
Miguel Ramos <miguel.ramos@msn.com>
2019-08-06 11:46:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // set up ======================================================================
 var express = require('express');
+const request = require('request');
 var app = express(); 						// create our app w/ express
 var mongoose = require('mongoose'); 				// mongoose for mongodb
 var port = process.env.PORT || 8081; 				// set the port
@@ -19,6 +20,10 @@ app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 // routes ======================================================================
 require('./server/routes.js')(app);
